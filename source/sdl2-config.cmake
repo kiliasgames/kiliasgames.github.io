cf79c8c45d725055a276ecfcfcce06aa684c7f0e
Kilias <kiliasgames@mail.com>
2023-10-06 07:29:45 +0400

    Update SDL 2 Examples, add copyright and disclaimer

--- source/sdl2-config.cmake
+++ source/sdl2-config.cmake
@@ -1,9 +1,16 @@
+# Copyright (C) 2022 Kilias
+
+cmake_minimum_required(VERSION 3.10)
+
 set(SDL2_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
 
-if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
-  set(SDL2_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2.lib;${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2main.lib")
-else ()
-  set(SDL2_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2.lib;${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2main.lib")
-endif ()
+if(${CMAKE_SIZEOF_VOID_P} STREQUAL "8")
+  set(sdl2_lib_type "x64")
+elseif(${CMAKE_SIZEOF_VOID_P} STREQUAL "4")
+  set(sdl2_lib_type "x86")
+else()
+  set(sdl2_lib_type "")
+  message(SEND_ERROR "Invalid CMAKE_SIZEOF_VOID_P")
+endif()
 
-string(STRIP "${SDL2_LIBRARIES}" SDL2_LIBRARIES)
+set(SDL2_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/${sdl2_lib_type}/SDL2.lib;${CMAKE_CURRENT_LIST_DIR}/lib/${sdl2_lib_type}/SDL2main.lib")
